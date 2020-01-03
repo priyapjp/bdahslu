d84ac32a12d95031d4d5c93b057e57cb50ce5b71
Lukas Gotschim <lukas@LukasMacBookPro.local>
2020-01-03 22:52:01 +0100

    scheduler updated

--- ccxt_scheduler.py
+++ ccxt_scheduler.py
@@ -3,8 +3,19 @@ import time
 from datetime import datetime
 import ccxt_worker as worker
 import logging
+import pickledb
 
 logging.basicConfig(filename='ccxt_app.log', level=logging.INFO)
+db = pickledb.load('ccxt_config.db', True)
+
+
+def write_start_date(start_date):
+    db.set('start_date', start_date)
+    db.dump()
+
+
+def get_last_start_date():
+    return db.get('start_date')
 
 
 def get_timestamp_now():
@@ -18,15 +29,24 @@ def job():
 
 
 def pull():
-    logging.info("----- Start pulling Data: " + get_timestamp_now())
-    from_date = '2019-12-31 00:00:00'
-    exchanges = ['kraken']
+    start_time = get_timestamp_now()
+    logging.info("----- Start pulling Data: " + start_time)
+    from_date = get_last_start_date()
+
+    if not from_date:
+        from_date = get_timestamp_now()
+        write_start_date(get_timestamp_now())
+
+    print("Last Start: " + from_date)
+
+    exchanges = ['kraken', 'binance', 'kucoin']
     for e in exchanges:
         worker.pull_data(e, from_date, 1000, '1m', '/Users/lukas/development')
-    logging.info("-----  Pulling Data completed: " + get_timestamp_now())
+        logging.info("-----  Pulling Data completed for : " + e + get_timestamp_now())
+    write_start_date(get_timestamp_now())
 
 
-schedule.every(3).hours.do(job)
+schedule.every(4).hours.do(job)
 
 while 1:
     schedule.run_pending()
